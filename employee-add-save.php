1cf4b6dd99327e6934c461f29606d9c9535a899e
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-05 14:01:55 -0500

    Update employee-add-save.php

--- employee-add-save.php
+++ employee-add-save.php
@@ -3,6 +3,26 @@
 <div class="alert alert-success" role="alert">
   Your employee has been added to the database!
 </div>
+<?php
+$servername = "localhost:3306";
+$username = "jeffreyn_user1";
+$password = "0w_zeP}]OVy0";
+$dbname = "jeffreyn_homework3";
+    
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$eName = $_POST["eName"];
+$sql = "INSERT into table Employee (employee_name) value (?)";
+    $stmt = $conn->prepare($sql);
+      $stmt->bind_params("s", $cid);
+    $stmt->execute();
+?>
+<a href="employees.php" class="btn btn-primary">Go back</a>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
