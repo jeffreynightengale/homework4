9543689364a29d5fb8e5e1340122ded2d14ce5ea
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-05 13:56:46 -0500

    Update employee-add-save.php

--- employee-add-save.php
+++ employee-add-save.php
@@ -1,1 +1,8 @@
-
+<?php require_once("header.php"); ?>
+<h1>Add Employee</h1>
+<div class="alert alert-success" role="alert">
+  Your employee has been added to the database!
+</div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
