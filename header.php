cb2ce88a40ab2954b0ae026f73023e9f08d44954
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-10 13:24:37 -0500

    Update header.php

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="customers.php">Customer Records</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Customer Records</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="employees.php">Employees</a>
