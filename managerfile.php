f52cc34037550fec6b2d41eeba38f6dc1d92b834
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-10 13:37:14 -0500

    Create managerfile.php

---
+++ managerfile.php
@@ -0,0 +1,44 @@
+<?php require_once("header.php"); ?>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Employees</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+$servername = "localhost:3306";
+$username = "jeffreyn_user1";
+$password = "0w_zeP}]OVy0";
+$dbname = "jeffreyn_homework3";
+    
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$cid = $_GET["id"];
+$sql = "SELECT employee_name FROM Employee e Where manager_id =" . $cid;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    ?>
+<tr>
+  <td><?=$row["employee_name"]?></td>
+</tr>
+  <?php
+    }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+</table>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
