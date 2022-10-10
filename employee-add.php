f74369bfc4fe19ca0ee52d5db3ea211ea7a33004
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-10 13:43:42 -0500

    Update employee-add.php

--- employee-add.php
+++ employee-add.php
@@ -5,6 +5,12 @@
     <label for="employeeName" class="form-label">Name</label>
     <input type="text" class="form-control" id="employeeName" aria-describedby="nameHelp" name="eName">
     <div id="nameHelp" class="form-text">Enter the employee's name</div>
+     <label for="managerID" class="form-label">ManagerID</label>
+    <input type="text" class="form-control" id="managerID" aria-describedby="nameHelp" name="mID">
+    <div id="nameHelp" class="form-text">Enter the manager's ID</div>
+     <label for="managerName" class="form-label">Manager Name</label>
+    <input type="text" class="form-control" id="managerName" aria-describedby="nameHelp" name="mName">
+    <div id="nameHelp" class="form-text">Enter the manager's name</div>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
