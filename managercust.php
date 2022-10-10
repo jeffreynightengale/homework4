4316fa718eab90735fe2a5a59afad01b5c695177
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-10 13:36:55 -0500

    Create managercust.php

---
+++ managercust.php
@@ -0,0 +1,51 @@
+<?php require_once("header.php"); ?>
+<h1>Manager's Customers</h1>
+<div class="card-group">
+    <?php
+$servername = "localhost:3306";
+$username = "jeffreyn_user1";
+$password = "0w_zeP}]OVy0";
+$dbname = "jeffreyn_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select manager_id, manager_name from Manager";
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["manager_name"]?></h5>
+      <p class="card-text"><ul>
+<?php
+    $section_sql = "select customer_name from Customer c join Employee e on e.employee_id = c.employee_id join Manager m on e.manager_id = m.manager_id where m.manager_id=" . $row["manager_id"];
+    $section_result = $conn->query($section_sql);
+    
+    while($section_row = $section_result->fetch_assoc()) {
+      echo "<li>" . $section_row["customer_name"] . "</li>";
+    }
+?>
+      </ul></p>
+  </div>
+    </div>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </card-group>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
